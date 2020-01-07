d79f4d344f5d0e54e9892cb2243fad6d241ea685
yazseyit77 <yazgeldi.seyidov@gmail.com>
2020-01-06 21:35:17 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
 
 
   //Your code here:
-
+  componentDidMount() {
+    this.handleAddTimer()
+  }
 
 
 
@@ -45,7 +47,7 @@ class App extends Component {
   // adds a random number for timer ID
   handleAddTimer = () => {
     this.setState(prevState => ({
-      timerIDs: [...prevState.timerIDs, Math.floor(Math.random()*1000)]
+      timerIDs: [...prevState.timerIDs, Math.floor(Math.random() * 1000)]
     }))
   }
 
